088ff9410a7056d5f6b2ee66164c6b1c833b7eec
Noah Rahm <correctsyntax@yahoo.com>
2024-02-26 18:25:12 -0600

    French -> Français

--- app/src/main/java/com/correctsyntax/biblenotify/LanguageSettings.java
+++ app/src/main/java/com/correctsyntax/biblenotify/LanguageSettings.java
@@ -34,7 +34,7 @@ public class LanguageSettings extends AppCompatActivity
 
     // Add languages
     languages.put("English", "en");
-    languages.put("French", "fr");
+    languages.put("Français", "fr");
 
     // Create an ArrayAdapter using the string array and a default spinner layout.
     ArrayAdapter<CharSequence> adapter =
